9d4f1ef401321f878596b1b0c588bd10be4b081c
avnercaleb <avnercaleb@hotmail.com>
2022-11-06 07:01:31 -0300

    Inserido metodo de criação de usuario

--- src/main/java/com/nelio/curso/controller/UserController.java
+++ src/main/java/com/nelio/curso/controller/UserController.java
@@ -1,13 +1,16 @@
 package com.nelio.curso.controller;
 
+import java.net.URI;
 import java.util.List;
-import java.util.Optional;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.nelio.curso.entities.User;
 import com.nelio.curso.service.UserService;
@@ -34,6 +37,14 @@ public class UserController {
 	public ResponseEntity<User> findById(@PathVariable Long id){
 		User user = service.findById(id);
 		return ResponseEntity.ok().body(user);
+	}
+	
+	@PostMapping
+	public ResponseEntity<User> insert(@RequestBody User user){
+		user = service.insert(user);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest()
+				.path("/{id}").buildAndExpand(user.getId()).toUri();
+		return ResponseEntity.created(uri).body(user);
 	}
 }
 	
